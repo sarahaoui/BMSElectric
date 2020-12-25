80e497ace2fbe8920d4aad642b46b366fc39281f
sara <aouinasara77@gmail.com>
2020-12-25 13:20:28 +0100

    signup and login confirmationn

--- app/src/main/java/com/example/bmselectric/Activity1.java
+++ app/src/main/java/com/example/bmselectric/Activity1.java
@@ -3,6 +3,7 @@ package com.example.bmselectric;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Activity1 extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
--- app/src/main/java/com/example/bmselectric/Main_Page.java
+++ app/src/main/java/com/example/bmselectric/Main_Page.java
@@ -3,6 +3,7 @@ package com.example.bmselectric;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Main_Page extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class Main_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__page);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
